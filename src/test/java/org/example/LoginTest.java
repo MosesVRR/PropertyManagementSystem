32290548b9a279c100a352a56a317d3f899fa8b6
Moses Rupak Raj Vedala <mosesvrr@gmail.com>
2024-03-15 16:10:22 -0400

    implement remaining code

---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginTest {
+
+    private static Main main = new Main();
+
+    @Test
+    void testAuthenticateSuccess() {
+//        assertTrue(main.login(System.in("admin"), "password"),
+//                "Authentication should succeed with correct credentials.");
+    }
+}
